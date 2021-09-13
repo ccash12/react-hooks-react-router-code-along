6850629b5fe9cdc7daa09f96a9bf847e7b8fe0f3
Courtney Cash <cashcourtney97@gmail.com>
2021-09-13 11:32:52 -0400

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { Route, Switch } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Login from "./Login";
+import NavBar from "./NavBar";
 
-function Home() {
+function App() {
   return (
     <div>
-      <h1>Home!</h1>
+      <NavBar />
+      <Switch>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
     </div>
   );
 }
 
-ReactDOM.render(<Home />, document.getElementById("root"));
+export default App;
